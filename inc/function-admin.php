3a4d3a957057321ade932e884eeb18616b4f6ace
Thiago <souza.ty@gmail.com>
2019-02-12 19:41:09 -0200

    Update functions
    - update function add_menu_page

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -9,11 +9,19 @@
 
 function heja_add_admin_page() {
 
-    add_menu_page( 'HEJA Theme Options', 'HEJA', 'manage_options', 'souzaty-heja', 'heja_theme_create_page', get_template_directory_uri() . '/img/admin-icon.png', 110  );
+    // Ganerate HEJA Admin Page
+    add_menu_page( 'HEJA Theme Options', 'HEJA', 'manage_options', 'souzaty_heja', 'heja_theme_create_page', get_template_directory_uri() . '/img/admin-icon.png', 110  );
+
+    // Ganerate HEJA Admin Sub Page
+    add_submenu_page( 'souzaty_heja', 'HEJA Theme Options', 'Contato', 'manage_options', 'souzaty_heja', 'heja_theme_create_page' );
+
+    add_submenu_page( 'souzaty_heja', 'HEJA Social Options', 'Redes Sociais', 'manage_options', 'souzaty_social', 'heja_theme_settings_page' );
 }
 add_action( 'admin_menu', 'heja_add_admin_page' );
 
 function heja_theme_create_page() {
     // Generation of admin page
-
+}
+function heja_theme_settings_page() {
+    // Generation of admin page
 }
