e4779af49be6bad57e16030f08d3e8c69e77d994
Thiago <souza.ty@gmail.com>
2019-02-13 12:13:10 -0200

    Add css admin file

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,3 +6,7 @@
     Admin Enqueue Functions
     =========================
 */
+
+function ses_load_admin_scripts() {
+    wp_register_style( 'ses_admin', get_template_directory_uri() . , '/css/ses.admin.css' );
+}
