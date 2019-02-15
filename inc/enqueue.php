423e1621fded53d5615c8d422fa9e4ac922d512a
Thiago <souza.ty@gmail.com>
2019-02-15 19:24:01 -0200

    update js function

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -13,6 +13,9 @@ function ses_load_admin_scripts( $hook ) {
 
     wp_register_style( 'ses_admin', get_template_directory_uri() . '/css/ses.admin.css', array(), '1.0.0', 'all' );
     wp_enqueue_style( 'ses_admin' );
+
+    wp_enqueue_media();
+
     wp_register_script( 'ses-admin-script', get_template_directory_uri() . '/js/ses.admin.js', array('jquery'), '1.0.0', true );
     wp_enqueue_script( 'ses-admin-script' );
 }
