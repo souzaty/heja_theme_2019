cfcc18dbcac19096c4a2763bd6aeaaf66ced18c4
Thiago <souza.ty@gmail.com>
2019-02-18 19:52:24 -0300

    add theme support

--- functions.php
+++ functions.php
@@ -44,6 +44,7 @@ function load_scripts() {
  // ========== Start Theme Admin
  require get_template_directory() . '/inc/function-admin.php';
  require get_template_directory() . '/inc/enqueue.php';
+ require get_template_directory() . '/inc/theme-support.php';
 
  // ========== Start Menu
  register_nav_menus(array( 'menu_header' => __('Menu topo Unidade', ''),
