34964abeb9218b75413bcf85d5450cef26e6ef05
Thiago <souza.ty@gmail.com>
2019-02-18 11:54:28 -0300

    Add header.php structure

--- header.php
+++ header.php
@@ -6,10 +6,79 @@
 */
  ?>
  <!DOCTYPE html>
- <html lang="pt-br">
+ <html lang="pt-br" style="margin:0 !important; padding:0; !important;">
  <head>
      <title><?php wp_title(''); ?></title>
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1">
+     <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/style.css" rel="stylesheet">
 <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>
+    <?php
+    $nomeUnidade = esc_attr( get_option ( 'nome_unidade' ) );
+    $email = esc_attr( get_option ( 'email_unidade' ) );
+    $ddd = esc_attr( get_option ( 'ddd_unidade' ) );
+    $tel = esc_attr( get_option ( 'tel_unidade' ) );
+    $endereco = esc_attr( get_option ( 'endereco_unidade' ) );
+    $cep = esc_attr( get_option ( 'cep_unidade' ) );
+    $cnpj = esc_attr( get_option ( 'cnpj_unidade' ) );
+    ?>
+  <!-- Navigation -->
+  <nav class="top-header">
+      <div class="container">
+        <a class="top-header-mail" href="#"><?php print $email ?></a>
+      </div>
+  </nav>
+  <nav class="navbar navbar-ses-navigation fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#">img aqui</a>
+      <button class="navbar-toggler navbar-toggler-right  bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+          <?php wp_nav_menu( array(
+
+              'menu' => 'menu_header',
+
+              'theme_location' => 'menu_header',
+
+              'container' => 'div',
+
+              'container_class' => 'collapse navbar-collapse',
+
+              'container_id' => 'navbarResponsive',
+
+              'menu_class' => 'nav-item mx-0 mx-lg-1',
+
+              'echo' => true,
+
+              'menu_id' => 'id_do_menu',
+
+              'before' => "",
+
+              'after' => "",
+
+              'link_before' => "",
+
+              'link_after' => "",
+
+              'depth' => 0,
+
+              'walker' => "",
+
+              ) );
+          ?>
+      </div>
+    </div>
+  </nav>
+  <!-- Header -->
+  <header class="masthead bg-primary text-white text-center">
+    <div class="container">
+      <h1 class=" mb-0">slider home</h1>
+      <h2 class="font-weight-light mb-0">texto descritivo inserir aqui</h2>
+      <a class="btn btn-xl btn-outline-light" href="#"> call to action</a>
+    </div>
+  </header>
