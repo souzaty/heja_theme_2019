0aef596022ef0bd1ef78ec85a44926ce0fa15f3f
Thiago <souza.ty@gmail.com>
2019-02-21 12:35:29 -0300

    change theme

--- footer.php
+++ footer.php
@@ -1,33 +1,33 @@
 <?php
 /**
-* @package WordPress
-* @subpackage SES - GO Unidades
-* @since SES - GO Unidades 1.0
-*/
-?>
-<?php
-$nomeUnidade = esc_attr( get_option ( 'nome_unidade' ) );
-$email = esc_attr( get_option ( 'email_unidade' ) );
-$ddd = esc_attr( get_option ( 'ddd_unidade' ) );
-$tel = esc_attr( get_option ( 'tel_unidade' ) );
-$endereco = esc_attr( get_option ( 'endereco_unidade' ) );
-$cep = esc_attr( get_option ( 'cep_unidade' ) );
-$cnpj = esc_attr( get_option ( 'cnpj_unidade' ) );
-?>
-<footer class="footer text-center" style="text-align:center; background:#cdcdcd; padding:20px  0">
-    <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <h2>inserir logos rodapé aqui</h2>
-          </div>
-        </div>
-    </div>
-    <div class="address">
-        <div class="container">
-            <p><?php print $endereco ?>, Goiás CEP: <?php print $cep ?></p>
-            <p>(<?php print $ddd ?>) <?php print $tel ?>, <?php print $email ?>, CNPJ <?php print $cnpj ?></p>
-        </div>
-    </div>
-</footer>
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Bootstrap_Starter
+ */
 
+?>
+<?php if(!is_page_template( 'blank-page.php' ) && !is_page_template( 'blank-page-with-container.php' )): ?>
+			</div><!-- .row -->
+		</div><!-- .container -->
+	</div><!-- #content -->
+    <?php get_template_part( 'footer-widget' ); ?>
+	<footer id="colophon" class="site-footer <?php echo wp_bootstrap_starter_bg_class(); ?>" role="contentinfo">
+		<div class="container pt-3 pb-3">
+            <div class="site-info">
+                &copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
+                <span class="sep"> | </span>
+                <a class="credits" href="https://afterimagedesigns.com/wp-bootstrap-starter/" target="_blank" title="Wordpress Technical Support" alt="Bootstrap Wordpress Theme"><?php echo esc_html__('Bootstrap Wordpress Theme','wp-bootstrap-starter'); ?></a>
+
+            </div><!-- close .site-info -->
+		</div>
+	</footer><!-- #colophon -->
+<?php endif; ?>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
 </body>
+</html>
--- header.php
+++ header.php
@@ -4,96 +4,83 @@
 * @subpackage SES - GO Unidades
 * @since SES - GO Unidades 1.0
 */
- ?>
- <!DOCTYPE html>
- <html style="margin:0 !important; padding:0 !important;" <?php language_attributes(); ?>>
- <head>
-     <title><?php bloginfo( 'name' ); wp_title(); ?></title>
-		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
-		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-     <link href="<?php echo esc_url( get_template_directory_uri() ); ?>/style.css" rel="stylesheet">
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
-<?php
-        $custom_css = esc_attr( get_option( 'sunset_css' ) );
-        if( !empty( $custom_css ) ):
-            echo '<style>' . $custom_css . '</style>';
-        endif;
-    ?>
 </head>
-<body <?php body_class(); ?> >
-    <?php
-    $logoUnidadeRodape = esc_attr( get_option ( 'logo_unidade_rodape' ) );
-    $nomeUnidade = esc_attr( get_option ( 'nome_unidade' ) );
-    $email = esc_attr( get_option ( 'email_unidade' ) );
-    $ddd = esc_attr( get_option ( 'ddd_unidade' ) );
-    $tel = esc_attr( get_option ( 'tel_unidade' ) );
-    $endereco = esc_attr( get_option ( 'endereco_unidade' ) );
-    $cep = esc_attr( get_option ( 'cep_unidade' ) );
-    $cnpj = esc_attr( get_option ( 'cnpj_unidade' ) );
-    ?>
-<!-- Navigation -->
-<nav class="ses-nav-top">
-	<div class="container">
-		<a class="ses-mail-top" href="mailto:<?php print $email ?>"><?php print $email ?></a>
-	</div>
-</nav>
-<nav class="navbar navbar-expand-lg ses-nav-default">
-	<div class="container">
-		<!-- Branding -->
-		<div class="image-container">
-			<div id="profile-picture" class="profile-picture" style="background-image: url(<?php print $logoUnidadeRodape; ?>);"></div>
-		</div>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-		</button>
-		<!-- Menu -->
-		<?php wp_nav_menu( array(
-			'menu' => 'menu_header',
-			'theme_location' => 'menu_header',
-			'container' => 'div',
-			'container_class' => 'collapse navbar-collapse ses-nav-menu navbar-right',
-			'container_id' => 'navbarSupportedContent',
-			'menu_class' => 'nav justify-content-end',
-			'echo' => true,
-			'menu_id' => 'id_do_menu',
-			'before' => "",
-			'after' => "",
-			'link_before' => "",
-			'link_after' => "",
-			'depth' => 0,
-			'walker' => "",
 
-			) );
-			?>
-	</div><!-- end container -->
-</nav><!-- end nav -->
-<!-- Header -->
-<header class="masthead bg-primary text-white text-center">
-<div class="container">
-  <h1 class=" mb-0">slider home</h1>
-  <h2 class="font-weight-light mb-0">texto descritivo inserir aqui</h2>
-  <a class="btn btn-xl btn-outline-light" href="#"> call to action</a>
-</div>
-</header>
-<div class="container">
-    <h3>Teste loop aqui</h3>
-            <?php
-            $new_query = new WP_Query( array(
-        'posts_per_page' => -1,
-        'post_type'      => especialidades,
-        'orderby'        => 'menu_order',
-        'paged'          => $paged
-        ) );
-
-        while ( $new_query->have_posts() ) : $new_query->the_post(); ?>
-
-        <li><?php the_title(''); ?></li>
-
+<body <?php body_class(); ?>>
         <?php
+        $nomeUnidade = esc_attr( get_option ( 'nome_unidade' ) );
+        $email = esc_attr( get_option ( 'email_unidade' ) );
+        $ddd = esc_attr( get_option ( 'ddd_unidade' ) );
+        $tel = esc_attr( get_option ( 'tel_unidade' ) );
+        $endereco = esc_attr( get_option ( 'endereco_unidade' ) );
+        $cep = esc_attr( get_option ( 'cep_unidade' ) );
+        $cnpj = esc_attr( get_option ( 'cnpj_unidade' ) );
+        ?>
 
-        endwhile;
-        wp_reset_postdata();
-            ?>
-</div>
+        <nav class="navbar navbar-expand-lg ses-nav-default">
+        	<div class="container">
+        		<!-- Branding -->
+        		<div class="image-container">
+        			<div id="profile-picture" class="profile-picture" style="background-image: url(<?php print $logoUnidadeRodape; ?>);"></div>
+        		</div>
+        		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        		<span class="navbar-toggler-icon"></span>
+        		</button>
+        		<!-- Menu -->
+        		<?php wp_nav_menu( array(
+        			'menu' => 'menu_header',
+        			'theme_location' => 'menu_header',
+        			'container' => 'div',
+        			'container_class' => 'collapse navbar-collapse ses-nav-menu navbar-right',
+        			'container_id' => 'navbarSupportedContent',
+        			'menu_class' => 'nav justify-content-end',
+        			'echo' => true,
+        			'menu_id' => 'id_do_menu',
+        			'before' => "",
+        			'after' => "",
+        			'link_before' => "",
+        			'link_after' => "",
+        			'depth' => 0,
+        			'walker' => "",
+
+        			) );
+        			?>
+        	</div><!-- end container -->
+        </nav><!-- end nav -->
+        <!-- Header -->
+        <header class="masthead bg-primary text-white text-center">
+        <div class="container">
+          <h1 class=" mb-0">slider home</h1>
+          <h2 class="font-weight-light mb-0">texto descritivo inserir aqui</h2>
+          <a class="btn btn-xl btn-outline-light" href="#"> call to action</a>
+        </div>
+        </header>
+        <div class="container">
+            <h3>Teste loop aqui</h3>
+                    <?php
+                    $new_query = new WP_Query( array(
+                'posts_per_page' => -1,
+                'post_type'      => especialidades,
+                'orderby'        => 'menu_order',
+                'paged'          => $paged
+                ) );
+
+                while ( $new_query->have_posts() ) : $new_query->the_post(); ?>
+
+                <li><?php the_title(''); ?></li>
+
+                <?php
+
+                endwhile;
+                wp_reset_postdata();
+                    ?>
+        </div>
