e26541afb0771cc248c5a50d4b5e324682c31cc3
Thiago <souza.ty@gmail.com>
2019-02-25 18:16:59 -0300

    Add templates part

--- template-parts/content.php
+++ template-parts/content.php
@@ -18,7 +18,7 @@
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="entry-title">abul<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
